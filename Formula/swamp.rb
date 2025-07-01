2ae99a0038aa824dbc53421eb2b4da2a6b134363
Peter Bjorklund <piot@hotmail.com>
2025-07-01 22:30:27 +0200

    chore: update to `0.1.17-alpha.23`

--- Formula/swamp.rb
+++ Formula/swamp.rb
@@ -1,16 +1,16 @@
 class Swamp < Formula
   desc "Swamp (Unsigned)"
   homepage "https://github.com/swamp/swamp"
-  version "0.1.17-alpha.19"
+  version "0.1.17-alpha.23"
 
 	on_arm do
     url "https://github.com/swamp/swamp/releases/download/v#{version}/swamp-darwin-arm64.tar.gz"
-    sha256 "8a037668d51e6914dd891205e7bdaf5c5e4f3d0e058d77c8864177bb018ddd69"
+    sha256 "08623caa001bc38f58937915387ffe1798f74ec2ffbbf00833793359d03bb266"
   end
 
   on_intel do
     url "https://github.com/swamp/swamp/releases/download/v#{version}/swamp-darwin-x86_64.tar.gz"
-    sha256 "5fbd4bf81cf9873c7c7de1fe63f006dfe2df841eef359b98088f090962d68e7c"
+    sha256 "9f53c8753bf9c9fc024b597f5e242b86704150088bf89c0534e7dbfbf0116757"
   end
 
   def install
