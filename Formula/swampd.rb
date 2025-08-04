5c3933dd64d6c20435ccfe0d07b3dc18da5183c8
Peter Bjorklund <piot@hotmail.com>
2025-08-04 16:18:38 +0200

    chore: swampd `0.0.2`

--- Formula/swampd.rb
+++ Formula/swampd.rb
@@ -1,17 +1,17 @@
 class Swampd < Formula
   desc "Swampd (Unsigned)"
   homepage "https://github.com/swamp/swampd"
-  version "0.0.1"
+  version "0.0.2"
 
 	on_macos do
 		on_arm do
 			url "https://github.com/swamp/swampd/releases/download/v#{version}/swampd-darwin-arm64.tar.gz"
-			sha256 "f97feca78f1dfbeee2312f1e40c1aa075cbe310eaa54e0b8b642904e8da6c5a8"
+			sha256 "d0a788d2f101b0bef5a22bf9aaf104c8827ad4cfd41b4e290bbf5effab261b04"
 		end
 
 		on_intel do
 			url "https://github.com/swamp/swampd/releases/download/v#{version}/swampd-darwin-x86_64.tar.gz"
-			sha256 "b70036517a6c7c19c86a7a0d78577a9f6949cce2b5fb9a32e7e1722817004636"
+			sha256 "5b08da6eb3bfcde09751d856aad0cf2f78417d273e76710db994174ee7e4f166"
 		end
 	end
 
