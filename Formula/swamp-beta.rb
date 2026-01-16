431cf0cd0c658d1024f7a848e1b54aa5678203ee
Peter Bjorklund <piot@hotmail.com>
2026-01-16 21:51:57 +0100

    chore: swamp-beta 0.3.3

--- Formula/swamp-beta.rb
+++ Formula/swamp-beta.rb
@@ -3,24 +3,29 @@ class SwampBeta < Formula
   homepage "https://github.com/swamp/swamp"
   license "MIT"
 
-  version "0.3.2"
+  version "0.3.3"
+
+  on_linux do
+    on_arm do
+      url "https://github.com/swamp/swamp/releases/download/v#{version}/swamp-#{version}-linux-aarch64.tar.gz"
+      sha256 "343128a29ab66b6b6b3d6b3d8a3e1bfd06d7a26f64ba13347b25366734fe9023"
+    end
+
+    on_intel do
+      url "https://github.com/swamp/swamp/releases/download/v#{version}/swamp-#{version}-linux-x86_64.tar.gz"
+      sha256 "70bf398ba8b1d147c7ec4a27c5e65e46d9bab43660d370dab67c94ce8a24d437"
+    end
+  end
 
   on_macos do
     on_arm do
       url "https://github.com/swamp/swamp/releases/download/v#{version}/swamp-#{version}-macos-arm64.tar.gz"
-      sha256 "ca1440043dff83c9f1e2e8e259ab41a74cfc8c2b51402246d34233d553da0f87"
+      sha256 "7181900a12ee9b474d8f360160e8814a6e5c36f2d4485e54af16f2665405addd"
     end
 
     on_intel do
       url "https://github.com/swamp/swamp/releases/download/v#{version}/swamp-#{version}-macos-x86_64.tar.gz"
-      sha256 "8b7382ec15eeec6ad09282ace8e06d5931ce09c78f4f7c3ee138ea815dcf8a03"
-    end
-  end
-
-  on_linux do
-    on_intel do
-      url "https://github.com/swamp/swamp/releases/download/v#{version}/swamp-#{version}-linux-x86_64.tar.gz"
-      sha256 "20a48ee68ce1a3340d15f3761983aad5583f271250f47430cd787abf293d8509"
+      sha256 "7b79bea03808d54449fc44fceec0dc864179da96c0a6ae5c50b28a35d2031068"
     end
   end
 
@@ -30,7 +35,7 @@ class SwampBeta < Formula
     arch = Hardware::CPU.arm? ? (OS.mac? ? "arm64" : "aarch64") : "x86_64"
 
     # version is not part of the executable inside the .tar.gz
-    binary_name = "swamp-#{os}-#{os_arch}"
+    binary_name = "swamp-#{os}-#{arch}"
 
     bin.install binary_name => "swamp-beta"
   end
