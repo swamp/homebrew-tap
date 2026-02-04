e77ec7d07545f2617c9ed9d559974b12ba863cea
Peter Bjorklund <piot@hotmail.com>
2026-02-04 10:56:31 +0100

    chore: bogd 0.0.2

--- Formula/bogd.rb
+++ Formula/bogd.rb
@@ -2,26 +2,27 @@ class Bogd < Formula
   desc "Bogd server"
   homepage "https://github.com/bogd-32/bogd"
 
-  version "0.0.1"
-
-  on_macos do
-    on_arm do
-      url "https://bogd.net/download/bogd-#{version}-macos-arm64.tar.gz"
-      sha256 "1b9826d91738e25cc60bfbe6351917958e6313b936c1bd5fbc29047fec20b573"
-    end
-
-    on_intel do
-      url "https://bogd.net/download/bogd-#{version}-macos-x86_64.tar.gz"
-      sha256 "642ce0b2b8c2d76c74e38f1961d607f464ab4445786c2206e357b4ff695dfab7"
-    end
-  end
+  version "0.0.2"
 
   on_linux do
     on_intel do
       url "https://bogd.net/download/bogd-#{version}-linux-x86_64.tar.gz"
-      sha256 "8a3e64540cbb0003370eebe3d5eb175f1f02ac3b459d9177e06f6cebf1a4f66b"
+      sha256 "3f8e9c014659c87f741bfa3b1fb5f6dc9cbffbeecbba4809946d8c68dc39be6c"
     end
   end
+
+  on_macos do
+    on_arm do
+      url "https://bogd.net/download/bogd-#{version}-macos-arm64.tar.gz"
+      sha256 "b32d6cb26363e815a50d298ca8a223d653c800c704dac3c51ab5f5a7b45b23da"
+    end
+
+    on_intel do
+      url "https://bogd.net/download/bogd-#{version}-macos-x86_64.tar.gz"
+      sha256 "b8182f3952cefecc52d4aa59e33742e97f29c52abb2bd818b055ab557a63bd96"
+    end
+  end
+
 
 def install
     os = OS.mac? ? "macos" : "linux"
