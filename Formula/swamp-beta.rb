4c5e84d79cc1c710f7af7b9f53bf1a4684514ccc
Peter Bjorklund <piot@hotmail.com>
2026-02-04 19:45:23 +0100

    chore: swamp-beta 0.3.10

--- Formula/swamp-beta.rb
+++ Formula/swamp-beta.rb
@@ -3,29 +3,29 @@ class SwampBeta < Formula
   homepage "https://github.com/swamp/swamp"
   license "MIT"
 
-  version "0.3.9"
+  version "0.3.10"
 
   on_linux do
     on_arm do
       url "https://github.com/swamp/swamp/releases/download/v#{version}/swamp-#{version}-linux-aarch64.tar.gz"
-      sha256 "9c0969a816676db944c46ec266e9575f94c9bcb34b49ab0b5121a7ea825109d1"
+      sha256 "9fd80c6c157a93d8dc61bcdd2b077abe16cc5eb67db5cbbbd6a6fc528d0b144b"
     end
 
     on_intel do
       url "https://github.com/swamp/swamp/releases/download/v#{version}/swamp-#{version}-linux-x86_64.tar.gz"
-      sha256 "f7a8528842bd75b3527840131295fefe406aefac6a19f0481124aca851aa9019"
+      sha256 "0dd7e66e54744b6ab01d8cb65a35fd455a28f5582648b5cb4f98260fa2630441"
     end
   end
 
   on_macos do
     on_arm do
       url "https://github.com/swamp/swamp/releases/download/v#{version}/swamp-#{version}-macos-arm64.tar.gz"
-      sha256 "886be852b1cb844938c0e7daf032d02c6288ab16f95ed32ce89274d2bb29880a"
+      sha256 "c66530e18e31b7209a1c6ad1272bedf0039551f76db99bdbabd21fed8da036b5"
     end
 
     on_intel do
       url "https://github.com/swamp/swamp/releases/download/v#{version}/swamp-#{version}-macos-x86_64.tar.gz"
-      sha256 "6a09041b8ad75abce6efe20e7ffcc136727eb91170adcb15908d6c679f7a1a80"
+      sha256 "0ef880008bb3b9a02f7156fe378ce2174438b79ed6eddd2d081d07bc4694559c"
     end
   end
 
